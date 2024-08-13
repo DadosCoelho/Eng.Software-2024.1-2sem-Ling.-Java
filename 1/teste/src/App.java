07873fa5a1a8ab355e91bb1be9c309c09a628970
DadosCoelho <daldocoelho@gmail.com>
2024-08-13 15:26:45 -0300

    2024-08-13

---
+++ 1/teste/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World, Boy!");
+
+        String primeiroNome = "Reinaldo";
+        String segundoNome = "Coelho";
+        String nomeCompleto = primeiroNome + " " + segundoNome;
+        System.out.println(nomeCompleto);
+        System.out.println(nomeCompleto.toUpperCase()); //Deixar as letras maiusculas
+        System.out.println(nomeCompleto.toLowerCase()); //Deixar as letras minusculas
+
+
+        String myStr = "Hello %s! %,d kilobyte is %,d bytes.";
+        String result = String.format(myStr, nomeCompleto, 1, 1024);
+        System.out.println(result);
+    }
+}
