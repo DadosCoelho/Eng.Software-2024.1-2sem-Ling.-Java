1ab3ac70f123fac213fb8c5375155f1e6cf5fbf2
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-14 09:31:36 -0300

    2024-08-13

---
+++ 1/teste/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World, Boy!");
+
+        String primeiroNome     = "Reinaldo";
+        String segundoNome      = "Fernandes";
+        String terceiroNome     = "Coelho";
+        String nomeCompleto     = primeiroNome + " " + segundoNome + " " + terceiroNome;
+        
+        System.out.println(nomeCompleto);
+        System.out.println(nomeCompleto.toUpperCase()); //Deixar as letras maiusculas
+        System.out.println(nomeCompleto.toLowerCase()); //Deixar as letras minusculas
+
+
+        String myStr = "Hello %s! %,d kilobyte is %,d bytes.";
+        String result = String.format(myStr, nomeCompleto, 1, 1024);
+        System.out.println(result);
+    }
+}
+
