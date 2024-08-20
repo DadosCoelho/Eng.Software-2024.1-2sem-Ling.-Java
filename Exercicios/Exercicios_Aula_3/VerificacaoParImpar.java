9314a21cc3c0c0e0509ef0ae29c5f8487bb46371
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-19 21:59:51 -0300

    Create VerificacaoParImpar.java

---
+++ Exercicios/Exercicios_Aula_3/VerificacaoParImpar.java
@@ -0,0 +1,13 @@
+package Exercicios.Exercicios_Aula_3;
+
+public class VerificacaoParImpar {
+    public static void main(String[] args) {
+        int numero = 7; // Suponha que queremos verificar o número 7
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par.");
+        } else {
+            System.out.println("O número é ímpar.");
+        }
+    }
+}
