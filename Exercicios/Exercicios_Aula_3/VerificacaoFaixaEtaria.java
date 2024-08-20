c64ac189e421680ffb6f91d7738bdc7bd6ecf6c5
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-19 21:59:45 -0300

    Create VerificacaoFaixaEtaria.java

---
+++ Exercicios/Exercicios_Aula_3/VerificacaoFaixaEtaria.java
@@ -0,0 +1,19 @@
+package Exercicios.Exercicios_Aula_3;
+
+public class VerificacaoFaixaEtaria {
+    public static void main(String[] args) {
+        int idade = 16; // Substitua pela idade que deseja verificar
+
+        if (idade >= 0 && idade <= 12) {
+            System.out.println("Você é uma criança.");
+        } else if (idade >= 13 && idade <= 17) {
+            System.out.println("Você é um adolescente.");
+        } else if (idade >= 18 && idade <= 64) {
+            System.out.println("Você é um adulto.");
+        } else if (idade >= 65) {
+            System.out.println("Você é um idoso.");
+        } else {
+            System.out.println("Idade inválida.");
+        }
+    }
+}
