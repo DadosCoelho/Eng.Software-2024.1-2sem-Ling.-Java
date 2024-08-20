7cbb7bdbc28de5eeb425af5d27c8fba408320ce6
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-19 21:59:38 -0300

    Create MediaNotas.java

---
+++ Exercicios/Exercicios_Aula_3/MediaNotas.java
@@ -0,0 +1,16 @@
+package Exercicios.Exercicios_Aula_3;
+
+public class MediaNotas {
+    public static void main(String[] args) {
+        // Suponha que as notas sejam 6, 4 e 8
+        double nota1 = 6;
+        double nota2 = 4;
+        double nota3 = 8;
+
+        // Calcula a média
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        // Exibe o resultado
+        System.out.println("A média das notas é: " + media);
+    }
+}
