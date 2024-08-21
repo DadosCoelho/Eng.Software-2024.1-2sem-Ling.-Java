1f0684f914befb1236ce90cd7411d7e118afe658
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-20 21:46:46 -0300

    Create Carro.java

---
+++ Exercicios/Aula_4/Carro.java
@@ -0,0 +1,13 @@
+package Exercicios.Aula_4;
+
+public class Carro {
+    static  String nome = "gol ";
+
+    public static void Ligar() {
+        System.out.println(nome + "Ligando!");
+    }
+
+    public void Desligar() {
+        System.out.println("Desligando!");
+    }
+}
