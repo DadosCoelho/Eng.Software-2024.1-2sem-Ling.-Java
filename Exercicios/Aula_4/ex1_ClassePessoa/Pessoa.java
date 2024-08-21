3e0e677d3eabe3b8be6c6d80b30deca417058e84
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-20 21:46:50 -0300

    Create Pessoa.java

---
+++ Exercicios/Aula_4/ex1_ClassePessoa/Pessoa.java
@@ -0,0 +1,23 @@
+package Exercicios.Aula_4.ex1_ClassePessoa;
+
+public class Pessoa {
+    static String nome = "Reinaldo";
+    static int idade = 26;
+    static String cidade = "Teresina";
+
+    public static void exibirInformacoes() {
+        System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos de idade e moro em " + cidade);
+    }   
+
+    public static void aniversario() {
+        int novaIdade;
+        novaIdade = idade + 1;
+        System.out.println("Antes eu tinha " + idade + " anos de idade, mas agora tenho " + novaIdade + " anos.");
+    }
+
+    public static void main(String[] args) {
+        exibirInformacoes();
+        aniversario();
+    }
+
+}
