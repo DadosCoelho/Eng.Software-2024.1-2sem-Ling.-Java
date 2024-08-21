ffa286328ae98a1a8d2d7d7ac542869e56db9d76
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-21 18:22:19 -0500

    Update Main.java

--- Exercicios/Aula_4/Main.java
+++ Exercicios/Aula_4/Main.java
@@ -3,8 +3,7 @@ package Exercicios.Aula_4;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Teste");
-        
-        
+
         Carro car = new Carro();
 
 
