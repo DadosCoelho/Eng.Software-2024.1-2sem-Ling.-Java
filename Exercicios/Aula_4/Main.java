322bd51dec118db22e6c1cabc96c903d8fda98a1
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-20 21:46:57 -0300

    Create Main.java

---
+++ Exercicios/Aula_4/Main.java
@@ -0,0 +1,24 @@
+package Exercicios.Aula_4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Teste");
+        
+        
+        Carro car = new Carro();
+
+
+        Comer();
+        //num.Voar();
+        Carro.Ligar();
+        car.Desligar();
+    }
+
+    public static void Comer() {
+        System.out.println("Comendo!");
+    }
+
+    public void Voar() {
+        System.out.println("Voando!");
+    }
+}
