5ef85906c19eb2d352bc24d75fe1101641d87b02
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-21 18:22:16 -0500

    Create Main.java

---
+++ Exercicios/Aula_4/ex2_ClasseLivro/Main.java
@@ -0,0 +1,25 @@
+package Exercicios.Aula_4.ex2_ClasseLivro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Livro MeuLivro = new Livro();
+
+        MeuLivro.titulo = "A volta dos que não foram";
+        MeuLivro.autor = "Desconhecido";
+        MeuLivro.numeroDePaginas = 657;
+
+        MeuLivro.detalhesDoLivro();
+
+        Livro MeuLivro2 = new Livro();
+
+        MeuLivro2.titulo = "A volta dos que foram";
+        MeuLivro2.autor = "Conhecido";
+        MeuLivro2.numeroDePaginas = 412;
+
+        MeuLivro2.detalhesDoLivro();
+
+
+    }
+
+}
