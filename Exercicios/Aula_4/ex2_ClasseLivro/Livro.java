0b65a0b7c84d72c06a15d245bbcddffd1776ad9c
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-21 18:22:12 -0500

    Update Livro.java

--- Exercicios/Aula_4/ex2_ClasseLivro/Livro.java
+++ Exercicios/Aula_4/ex2_ClasseLivro/Livro.java
@@ -1,17 +1,15 @@
 package Exercicios.Aula_4.ex2_ClasseLivro;
 
 public class Livro {
-    static String titulo = "A volta dos que não foram";
-    static String autor = "Desconhecido";
-    static int numeroDePaginas = 657;
+    static String titulo;
+    static String autor;
+    static int numeroDePaginas;
 
     public static void detalhesDoLivro() {
         System.out.println(" Titulo  = " + titulo);
         System.out.println(" Autor   = " + autor);
         System.out.println(" Num Pag = " + numeroDePaginas + " páginas");
     }
-
-
 
     public static void main(String[] args) {
         detalhesDoLivro();
