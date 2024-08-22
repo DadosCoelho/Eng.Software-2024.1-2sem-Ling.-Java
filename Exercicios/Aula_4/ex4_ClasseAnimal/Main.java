2646f199e057ad5a08cc8af30c1c3053a6222f9c
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-21 19:34:16 -0500

    Create Main.java

---
+++ Exercicios/Aula_4/ex4_ClasseAnimal/Main.java
@@ -0,0 +1,25 @@
+package Exercicios.Aula_4.ex4_ClasseAnimal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal Animal = new Animal();
+
+        Animal.nome = "Bili";
+        Animal.especie = "Desconhecido";
+        Animal.idade = 4;
+
+        Animal.exibirAnimal();
+        Animal.aniversario();
+        Animal.ehAduto();
+
+        Animal Animal2 = new Animal();
+
+        Animal2.nome = "Lulu";
+        Animal2.especie = "Vira lata";
+        Animal2.idade = 4;
+
+        Animal2.exibirAnimal();
+        Animal.ehAduto();
+    }
+}
