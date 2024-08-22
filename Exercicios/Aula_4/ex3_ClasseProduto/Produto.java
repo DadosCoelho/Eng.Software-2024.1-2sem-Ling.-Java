668f5c6a42edd3129a47d1ca64cb02b82a7e6ed3
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-21 19:10:50 -0500

    Create Produto.java

---
+++ Exercicios/Aula_4/ex3_ClasseProduto/Produto.java
@@ -0,0 +1,25 @@
+package Exercicios.Aula_4.ex3_ClasseProduto;
+
+public class Produto {
+    static String nome;
+    static float preco;
+    static int quantidade;
+    static int quantidadeVendida;
+    static int quantidadeAnterior;
+
+    public static void exibirProduto() {
+        System.out.println(" Nome       = " + nome);
+        System.out.println(" Preco      = " + preco);
+        System.out.println(" Quantidade = " + quantidade);
+    }
+
+    public static void vender(int quantidadeVendida) {
+        quantidadeAnterior = quantidade;
+        if (quantidadeVendida < quantidade) {
+            quantidade = quantidade - quantidadeVendida;
+        }
+        System.out.println(" Quantidade anterior = " + quantidadeAnterior);
+        System.out.println(" Quantidade vendida = " + quantidadeVendida);
+        System.out.println(" Quantidade atual = " + quantidade);
+    }
+}
