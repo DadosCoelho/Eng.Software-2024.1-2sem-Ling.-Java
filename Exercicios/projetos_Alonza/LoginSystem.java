543032f850ce48e7d87b9c448dc8d7b4bbbd7085
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-24 09:21:51 -0300

    Create LoginSystem.java

---
+++ Exercicios/projetos_Alonza/LoginSystem.java
@@ -0,0 +1,25 @@
+package Exercicios.projetos_Alonza;
+
+import java.util.Scanner;
+
+public class LoginSystem {
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      String username = "admin";
+      String password = "password";
+
+      System.out.print("Digite o nome de usuário: ");
+      String inputUsername = scanner.nextLine();
+
+      System.out.print("Digite a senha: ");
+      String inputPassword = scanner.nextLine();
+
+      if(inputUsername.equals(username) && inputPassword.equals(password)) {
+         System.out.println("Login bem sucedido.");
+      } else {
+         System.out.println("Nome de usuário ou senha incorretos.");
+      }
+
+      scanner.close();
+   }
+}
