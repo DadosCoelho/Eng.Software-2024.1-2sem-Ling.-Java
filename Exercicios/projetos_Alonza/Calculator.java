81d7673843f055289b711586b94c312e6bb70acc
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-26 19:28:11 -0300

    Update Calculator.java

--- Exercicios/projetos_Alonza/Calculator.java
+++ Exercicios/projetos_Alonza/Calculator.java
@@ -4,32 +4,28 @@ import java.util.Scanner;
 
 public class Calculator {
    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      System.out.print("Digite o primeiro número: ");
-      double num1 = scanner.nextDouble();
-      System.out.print("Digite o segundo número: ");
-      double num2 = scanner.nextDouble();
-      System.out.print("Digite a operação (+, -, *, /): ");
-      char operator = scanner.next().charAt(0);
-      scanner.close();
+       double num1;
+       double num2;
+       char operator;
+       try (Scanner scanner = new Scanner(System.in)) {
+           System.out.print("Digite o primeiro número: ");
+           num1 = scanner.nextDouble();
+           System.out.print("Digite o segundo número: ");
+           num2 = scanner.nextDouble();
+           System.out.print("Digite a operação (+, -, *, /): ");
+           operator = scanner.next().charAt(0);
+       }
       double result;
 
       switch(operator) {
-         case '+':
-            result = num1 + num2;
-            break;
-         case '-':
-            result = num1 - num2;
-            break;
-         case '*':
-            result = num1 * num2;
-            break;
-         case '/':
-            result = num1 / num2;
-            break;
-         default:
-            System.out.println("Operação inválida.");
-            return;
+         case '+' -> result = num1 + num2;
+         case '-' -> result = num1 - num2;
+         case '*' -> result = num1 * num2;
+         case '/' -> result = num1 / num2;
+         default -> {
+             System.out.println("Operação inválida.");
+             return;
+           }
       }
 
       System.out.println(num1+" "+operator+" "+num2+": "+result);
