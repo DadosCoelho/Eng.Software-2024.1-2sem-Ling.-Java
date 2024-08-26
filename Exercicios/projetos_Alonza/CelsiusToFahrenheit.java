b547ca61f17a8a010f2018ddeef3a483714e3966
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-26 19:28:14 -0300

    Update CelsiusToFahrenheit.java

--- Exercicios/projetos_Alonza/CelsiusToFahrenheit.java
+++ Exercicios/projetos_Alonza/CelsiusToFahrenheit.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class CelsiusToFahrenheit {
    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      System.out.print("Digite a temperatura em Celsius: ");
-      double celsius = scanner.nextDouble();
-      scanner.close();
+       double celsius;
+       try (Scanner scanner = new Scanner(System.in)) {
+           System.out.print("Digite a temperatura em Celsius: ");
+           celsius = scanner.nextDouble();
+       }
       double fahrenheit = (celsius * 9/5) + 32;
       System.out.println(celsius+" graus Celsius é igual a "+fahrenheit+" graus Fahrenheit.");
    }
