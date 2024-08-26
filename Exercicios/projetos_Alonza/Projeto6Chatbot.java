871675837fed299ab023ea1fadb72dc7d8afe3e9
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-26 19:28:20 -0300

    Update Projeto6Chatbot.java

--- Exercicios/projetos_Alonza/Projeto6Chatbot.java
+++ Exercicios/projetos_Alonza/Projeto6Chatbot.java
@@ -6,22 +6,20 @@ public class Projeto6Chatbot {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite o número de termos que deseja somar: ");
-        int n = sc.nextInt();
-
-        double soma = 0;
-        int sinal = 1;
-
-        for (int i = 1; i <= n; i++) {
-            soma += sinal * (1.0 / i);
-            sinal *= -1;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite o número de termos que deseja somar: ");
+            int n = scanner.nextInt();
+            
+            double soma = 0;
+            int sinal = 1;
+            
+            for (int i = 1; i <= n; i++) {
+                soma += sinal * (1.0 / i);
+                sinal *= -1;
+            }
+            
+            System.out.println("A soma dos " + n + " primeiros termos da série é: " + soma);
         }
-
-        System.out.println("A soma dos " + n + " primeiros termos da série é: " + soma);
-
-        sc.close();
     }
 
 }
