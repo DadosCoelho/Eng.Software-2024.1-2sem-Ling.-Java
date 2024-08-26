3fac7f950d75d391a297f1b243a9ba9a4b912d45
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-26 19:28:17 -0300

    Update LoginSystem.java

--- Exercicios/projetos_Alonza/LoginSystem.java
+++ Exercicios/projetos_Alonza/LoginSystem.java
@@ -4,22 +4,20 @@ import java.util.Scanner;
 
 public class LoginSystem {
    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      String username = "admin";
-      String password = "password";
-
-      System.out.print("Digite o nome de usuário: ");
-      String inputUsername = scanner.nextLine();
-
-      System.out.print("Digite a senha: ");
-      String inputPassword = scanner.nextLine();
-
-      if(inputUsername.equals(username) && inputPassword.equals(password)) {
-         System.out.println("Login bem sucedido.");
-      } else {
-         System.out.println("Nome de usuário ou senha incorretos.");
-      }
-
-      scanner.close();
+       try (Scanner scanner = new Scanner(System.in)) {
+           String username = "admin";
+           String password = "password";
+           
+           System.out.print("Digite o nome de usuário: ");
+           String inputUsername = scanner.nextLine();
+           
+           System.out.print("Digite a senha: ");
+           String inputPassword = scanner.nextLine();
+           
+           if(inputUsername.equals(username) && inputPassword.equals(password)) {
+               System.out.println("Login bem sucedido.");
+           } else {
+               System.out.println("Nome de usuário ou senha incorretos.");
+           }}
    }
 }
