be65ba41a7b6d62f243f87311332e5e58ac842a8
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-08-27 21:47:43 -0300

    Update Main.java

--- Exercicios/Aula_4/ex2_ClasseLivro/Main.java
+++ Exercicios/Aula_4/ex2_ClasseLivro/Main.java
@@ -1,24 +1,26 @@
 package Exercicios.Aula_4.ex2_ClasseLivro;
 
 public class Main {
-    @SuppressWarnings("static-access")
     public static void main(String[] args) {
 
-        Livro MeuLivro = new Livro();
+        Livro MeuLivro = new Livro("Senhor dos aneis", 25);
 
-        MeuLivro.titulo = "A volta dos que não foram";
+        /*MeuLivro.titulo = "A volta dos que não foram";
         MeuLivro.autor = "Desconhecido";
-        MeuLivro.numeroDePaginas = 657;
+        MeuLivro.numeroDePaginas = 657;*/
 
         MeuLivro.detalhesDoLivro();
 
-        Livro MeuLivro2 = new Livro();
+        Livro MeuLivro2 = new Livro("A regra do jogo", 20);
 
-        MeuLivro2.titulo = "A volta dos que foram";
+        /*MeuLivro2.titulo = "A volta dos que foram";
         MeuLivro2.autor = "Conhecido";
-        MeuLivro2.numeroDePaginas = 412;
+        MeuLivro2.numeroDePaginas = 412;*/
 
         MeuLivro2.detalhesDoLivro();
+
+        System.out.println(MeuLivro.compararLivros(MeuLivro2));
+        System.out.println(MeuLivro2.compararLivros(MeuLivro));
     }
 
 }
