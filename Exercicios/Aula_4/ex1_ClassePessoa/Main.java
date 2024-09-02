e38402248f2208f446169e06d4da5247e0d95be1
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-02 16:57:42 -0500

    Update Main.java

--- Exercicios/Aula_4/ex1_ClassePessoa/Main.java
+++ Exercicios/Aula_4/ex1_ClassePessoa/Main.java
@@ -1,10 +1,9 @@
 package Exercicios.Aula_4.ex1_ClassePessoa;
 
 public class Main {
-    @SuppressWarnings("static-access")
     public static void main(String[] args) {
         Pessoa Pessoa1 = new Pessoa();
-        Pessoa Pessoa2 = new Pessoa();
+        //Pessoa Pessoa2 = new Pessoa();
 
         Pessoa1.nome = "Reinaldo";
         Pessoa1.cidade = "Teresina-PI";
