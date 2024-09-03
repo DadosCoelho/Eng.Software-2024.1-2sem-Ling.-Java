7b7441680a435b608a046fe1cd09b1692df6df20
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-02 19:14:33 -0500

    Create Main.java

---
+++ Exercicios/Aula_6/ex3_ControleEstoque/Main.java
@@ -0,0 +1,16 @@
+package Exercicios.Aula_6.ex3_ControleEstoque;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ControleEstoque item1 = new ControleEstoque("Caderno");
+
+        item1.detalheEstoque();
+        item1.adicionarEstoque(50);
+        item1.detalheEstoque();
+        item1.removerEstoque(10);
+        item1.detalheEstoque();
+
+    }
+    
+}
