fc7a26bdd11b484f8e41f028002dc6661f3cbdfc
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-04 14:36:22 -0300

    Update Main.java

--- Exercicios/Aula_6/ex5_GerenciamentoCursos/Main.java
+++ Exercicios/Aula_6/ex5_GerenciamentoCursos/Main.java
@@ -2,12 +2,18 @@ package Exercicios.Aula_6.ex5_GerenciamentoCursos;
 
 public class Main {
     public static void main(String[] args) {
-        GerenciamentoCursos Curso1 = new GerenciamentoCursos("POO",15,10);
+        GerenciamentoCursos Curso1 = new GerenciamentoCursos("POO",15,2);
 
         Curso1.exibirInformacoes();
         Curso1.matricularAluno();
         Curso1.exibirInformacoes();
         Curso1.cancelarMatriculas();
+        Curso1.exibirInformacoes();
+        Curso1.matricularAluno();
+        Curso1.exibirInformacoes();
+        Curso1.matricularAluno();
+        Curso1.exibirInformacoes();
+        Curso1.matricularAluno();
         Curso1.exibirInformacoes();
 
     }
