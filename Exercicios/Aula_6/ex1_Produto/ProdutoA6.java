9015a0c9dc32377c0542a2f6812a124a933056fc
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-03 21:49:51 -0300

    Update ProdutoA6.java

--- Exercicios/Aula_6/ex1_Produto/ProdutoA6.java
+++ Exercicios/Aula_6/ex1_Produto/ProdutoA6.java
@@ -3,6 +3,7 @@ package Exercicios.Aula_6.ex1_Produto;
 public class ProdutoA6 {
     public String nome;
     private double preco;
+    //private int idProduto;
 
     public ProdutoA6(double preco){
         this.preco = preco;
@@ -18,11 +19,34 @@ public class ProdutoA6 {
     public void aplicarDesconto(double porcentagem){
         if(porcentagem >= 10 && porcentagem <= 90){    
             double desconto = porcentagem / 100;
-            preco = preco - (preco * desconto);
+
+            setPreco(getPreco() - (getPreco() * desconto));
+
+            //preco = preco - (preco * desconto);
             System.out.println(" ");
             System.out.println("Aplicado desconto de: " + (desconto*100) + "%");
         } else{
         System.out.println("Valor Invalido!");
         }
+    }
+
+    public double getPreco(){
+        return this.preco;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setPreco(double preco){
+        //if(preco >=100){
+        this.preco = preco;
+        /*} else {
+            System.out.println("Valor Invalido!");
+        }*/
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
     }
 }
