15c79be862e8265ae73b13cd461409e94918e7ec
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-06 18:01:00 -0300

    Create Main.java

---
+++ Exercicios/Aula_08/ex1_ControleFuncionario/Main.java
@@ -0,0 +1,12 @@
+package Exercicios.Aula_08.ex1_ControleFuncionario;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcionario funcionario1 = new Funcionario("Reinaldo", "Estagiario", 0);
+
+        funcionario1.promover("Progamador Junior", 2500);
+        
+
+
+    }
+}
