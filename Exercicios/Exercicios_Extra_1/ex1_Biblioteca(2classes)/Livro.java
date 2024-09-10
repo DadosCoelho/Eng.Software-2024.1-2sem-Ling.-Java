313297ed936c63cf22d9245e91de11d4f46d8409
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-10 15:04:22 -0300

    Create Livro.java

---
+++ Exercicios/Exercicios_Extra_1/ex1_Biblioteca(2classes)/Livro.java
@@ -0,0 +1,36 @@
+package Exercicios.Exercicios_Extra_1.ex1_Biblioteca(2classes);
+
+public class Livro {
+    String titulo;
+    String autor;
+    String isbn;
+    boolean disponivel;
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public boolean getDisponivel(){
+        return disponivel;
+    }
+
+    public void emprestar(){
+
+    }
+
+
+
+
+}
