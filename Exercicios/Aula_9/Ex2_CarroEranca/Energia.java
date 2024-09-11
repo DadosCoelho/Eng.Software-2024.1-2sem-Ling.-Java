7881d86ec72a087abdf02c951a6f5e9918dfd598
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-10 21:56:50 -0300

    Create Energia.java

---
+++ Exercicios/Aula_9/Ex2_CarroEranca/Energia.java
@@ -0,0 +1,9 @@
+package Exercicios.Aula_9.Ex2_CarroEranca;
+
+public class Energia extends CarroA9 {
+
+    public void ApresentacaoEnergia(){
+        System.out.println("O Carro "+nome+" da marca "+marca+" é movido a energia eletrica!");
+    }
+    
+}
