eac647e819ee3909e5ced386939b93c819f2aecc
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-10 21:56:53 -0300

    Create Gasolina.java

---
+++ Exercicios/Aula_9/Ex2_CarroEranca/Gasolina.java
@@ -0,0 +1,9 @@
+package Exercicios.Aula_9.Ex2_CarroEranca;
+
+public class Gasolina extends CarroA9 {
+
+    public void ApresentacaoGasolina(){
+        System.out.println("O Carro "+nome+" da marca "+marca+" é movido a gasolina!");
+    }
+    
+}
