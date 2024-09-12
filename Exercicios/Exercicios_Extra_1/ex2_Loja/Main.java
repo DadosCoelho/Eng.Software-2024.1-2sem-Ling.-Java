d352df435431cf562dac76eebb88a9c1d008162f
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-12 11:53:20 -0300

    Update Main.java

--- Exercicios/Exercicios_Extra_1/ex2_Loja/Main.java
+++ Exercicios/Exercicios_Extra_1/ex2_Loja/Main.java
@@ -1,40 +1,21 @@
-package Exercicios.Exercicios_Extra_1.ex2_Loja;
-
 public class Main {
     public static void main(String[] args) {
-        
-        //LojaE1 loja = new LojaE1();
+        LojaE1 loja = new LojaE1();
 
-        // Adicionando produtos
-        ProdutoE1 produto1 = new ProdutoE1("001", "Caneta", 2, 100);
-        //ProdutoE1 produto2 = new ProdutoE1("002", "Caderno", 15,50);
-        
-        System.out.println("Nome: " + produto1.getNome());
-        
-        
-        /*
+        ProdutoE1 produto1 = new ProdutoE1("Arroz", "001", 5.50, 100);
+        ProdutoE1 produto2 = new ProdutoE1("Feijão", "002", 7.00, 50);
 
         loja.adicionarProduto(produto1);
         loja.adicionarProduto(produto2);
-        
-        // Buscando produtos
-        System.out.println("Buscando por nome 'Caneta': " + loja.buscarProdutosPorNome("Caneta"));
-        System.out.println("Buscando por código '002': " + loja.buscarProdutoPorCodigo("002"));
 
-        // Vendendo produtos
-        if (loja.venderProduto("001", 10)) {
-            System.out.println("Venda realizada com sucesso!");
-        } else {
-            System.out.println("Falha na venda.");
-        }
+        System.out.println("Produtos com nome 'Arroz': " + loja.buscarProdutoPorNome("Arroz"));
+        System.out.println("Produto com código '002': " + loja.buscarProdutoPorCodigo("002"));
 
-        // Removendo produtos
-        if (loja.removerProduto("002")) {
-            System.out.println("Produto removido com sucesso!");
-        } else {
-            System.out.println("Produto não encontrado.");
-        }
-            
-        */
+        loja.venderProduto("001", 10);
+        System.out.println("Quantidade de 'Arroz' após venda: " + produto1.getQuantidade());
+
+        loja.removerProduto("002");
+        System.out.println("Produto com código '002' após remoção: " + loja.buscarProdutoPorCodigo("002"));
     }
 }
+
