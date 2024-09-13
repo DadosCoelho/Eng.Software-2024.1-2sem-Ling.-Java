5b03207d2e633bae5a1c2b4ec6e4c4b07a0b6d29
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-13 09:46:14 -0300

    Create Main.java

---
+++ Exercicios/Exercicios_Extra_2/ex1_GestãoFuncionarios/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        EmpresaE2 empresa = new EmpresaE2();
+
+        FuncionarioHoristaE2 horista = new FuncionarioHoristaE2("João", "123456789", 0, 160, 50);
+        FuncionarioMensalistaE2 mensalista = new FuncionarioMensalistaE2("Maria", "987654321", 0, 3000);
+
+        empresa.adicionarFuncionario(horista);
+        empresa.adicionarFuncionario(mensalista);
+
+        System.out.println("Folha de pagamento: " + empresa.calcularFolhaDePagamento());
+
+        ArrayList<FuncionarioE2> funcionarios = empresa.buscarFuncionarioPorNome("João");
+        for (FuncionarioE2 funcionario : funcionarios) {
+            System.out.println("Funcionário encontrado: " + funcionario.getNome());
+        }
+
+        empresa.removerFuncionario("123456789");
+        System.out.println("Folha de pagamento após remoção: " + empresa.calcularFolhaDePagamento());
+    }
+}
