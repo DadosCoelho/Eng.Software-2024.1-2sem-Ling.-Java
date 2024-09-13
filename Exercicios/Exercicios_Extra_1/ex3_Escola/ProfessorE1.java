596bc1190c81587eaba2466a90a3cce53b4f75df
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-12 21:02:53 -0300

    Create ProfessorE1.java

---
+++ Exercicios/Exercicios_Extra_1/ex3_Escola/ProfessorE1.java
@@ -0,0 +1,24 @@
+public class ProfessorE1 {
+    private String nome;
+    private String cpf;
+    private String especialidade;
+
+    public ProfessorE1(String nome, String cpf, String especialidade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.especialidade = especialidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+}
+
