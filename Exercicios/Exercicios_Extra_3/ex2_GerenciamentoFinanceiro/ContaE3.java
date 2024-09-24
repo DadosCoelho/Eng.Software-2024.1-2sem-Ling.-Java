4977fa5ea47917d566ef4b7a4ac51d73f0cd3546
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-09-23 21:08:18 -0300

    Create ContaE3.java

---
+++ Exercicios/Exercicios_Extra_3/ex2_GerenciamentoFinanceiro/ContaE3.java
@@ -0,0 +1,33 @@
+abstract class ContaE3 {
+    private String numeroConta;
+    private String titular;
+    protected double saldo;
+
+    public ContaE3(String numeroConta, String titular, double saldo) {
+        this.numeroConta = numeroConta;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    public abstract void calcularSaldo();
+}
