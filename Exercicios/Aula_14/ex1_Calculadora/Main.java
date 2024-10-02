0d88e6aa0f8404b5beb30c00312ab24816f5bf5f
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-01 21:50:53 -0300

    Create Main.java

---
+++ Exercicios/Aula_14/ex1_Calculadora/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        CalculadoraA14E1 calc = new CalculadoraA14E1();
+
+        System.out.println(calc.Soma(4,5));
+        System.out.println(calc.Soma(4,6,8));
+    }
+}
