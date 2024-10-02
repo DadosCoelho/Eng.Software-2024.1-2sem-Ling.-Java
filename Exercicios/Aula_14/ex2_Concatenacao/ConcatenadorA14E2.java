d0dfb8582473f5167822d736abdd213c616b62ec
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-01 21:50:56 -0300

    Create ConcatenadorA14E2.java

---
+++ Exercicios/Aula_14/ex2_Concatenacao/ConcatenadorA14E2.java
@@ -0,0 +1,9 @@
+public class ConcatenadorA14E2 {
+    public String Concatenar(String TextA, String TextB){
+        return TextA + TextB;
+    }
+
+    public String Concatenar(String TextA, String TextB, String TextC){
+        return TextA + TextB + TextC;
+    }
+}
