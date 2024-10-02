d4f54cae7b1090f7fa41731a302e668b7f02f7e6
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-01 21:51:01 -0300

    Create FormaA14E3.java

---
+++ Exercicios/Aula_14/ex3_AreaDeFormas/FormaA14E3.java
@@ -0,0 +1,9 @@
+public class FormaA14E3 {
+    public double CalcularArea(double base, double altura){
+        return base * altura;
+    }
+
+    public double CalcularArea(double lado){
+        return lado * lado;
+    }
+}
