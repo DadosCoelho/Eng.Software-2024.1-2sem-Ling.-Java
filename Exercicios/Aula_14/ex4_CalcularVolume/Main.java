68b27cfc1dd88ecb0c6d46c91c76e904d58b83ef
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-01 21:51:06 -0300

    Create Main.java

---
+++ Exercicios/Aula_14/ex4_CalcularVolume/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        VolumeA14E4 volume = new VolumeA14E4();
+
+        System.out.println(volume.calcularVolume(3,5));
+
+    }
+}
