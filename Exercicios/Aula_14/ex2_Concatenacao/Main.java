ec8d80fd9a09f95e8fdf22a204f83011adff5bae
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-01 21:50:58 -0300

    Create Main.java

---
+++ Exercicios/Aula_14/ex2_Concatenacao/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ConcatenadorA14E2 Concat = new ConcatenadorA14E2();
+
+        System.out.println(Concat.Concatenar("Sou", "Brasileiro"));
+        System.out.println(Concat.Concatenar("Sou", "Brasileiro","Raiz"));
+    }
+}
