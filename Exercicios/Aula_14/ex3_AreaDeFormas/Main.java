a857d453291118a3942687ce0f6623ffa37a12fd
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-01 21:51:03 -0300

    Create Main.java

---
+++ Exercicios/Aula_14/ex3_AreaDeFormas/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FormaA14E3 forma = new FormaA14E3();
+
+        System.out.println(forma.CalcularArea(5));
+        System.err.println(forma.CalcularArea(3, 2));
+    }
+}
