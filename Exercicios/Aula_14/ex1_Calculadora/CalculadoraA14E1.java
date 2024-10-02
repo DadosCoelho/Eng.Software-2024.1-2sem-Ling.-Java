365d4dd81c989050a8e2a5ec1454a5496847815a
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-01 21:50:50 -0300

    Create CalculadoraA14E1.java

---
+++ Exercicios/Aula_14/ex1_Calculadora/CalculadoraA14E1.java
@@ -0,0 +1,9 @@
+public class CalculadoraA14E1 {
+    public double Soma(double x, double y){
+        return x + y;
+    }
+
+    public double Soma(double x, double y, double z){
+        return x + y + z;
+    }
+}
