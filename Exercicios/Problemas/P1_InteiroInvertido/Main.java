5dc894bdab483bc64bffa9985e646df42fc416d7
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2024-10-09 16:42:33 -0300

    Create Main.java

---
+++ Exercicios/Problemas/P1_InteiroInvertido/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        InteiroInvertidoP1 solution = new InteiroInvertidoP1();
+
+        System.out.println(solution.reverse(123));
+        System.out.println(solution.reverse(-123));
+        System.out.println(solution.reverse(120));
+    }
+}
